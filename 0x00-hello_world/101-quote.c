666fd32c8dcfb2938dc2388bc92fb5c2cafd2b6a
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-17 16:48:44 -0700

    Intel and quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main = print and that piece of art is useful" - 
+ * Dora Korpar, 2015-10-19
+ *
+ *
+ *
+ *
+ */
+int main (void)
+{
+  printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+}
+Return (0);
+done
