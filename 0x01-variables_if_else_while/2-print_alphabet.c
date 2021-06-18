277c19cc7ec24da745213dc3a370eb68b96a053f
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-18 20:21:12 +0100

    Errors

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <ctype.h>
 /**
- * main - prints the alphabet in lowercase 
+ * main - prints the alphabet in lowercase
  * Return: 0
  */
 
 int main(void)
 {
 	char ch;
+
 	for(ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
-	putchar('\n');
-    }
-    return 0;
+	{
+	putchar(ch);
+	}
+	return (0);
 }
