de00c5c0262fd122428347af86d198be43420f56
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-20 22:24:17 +0100

    Fix errors

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,28 +9,28 @@ int main(void)
 {
 	int i;
 	int j;
-    int k;
+	int k;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = i + 1; j <= 9; j++)
 		{
-            for (k = j + 1; k <= 9; k++)
-            {
-                if (i != j)
-                {
-                    putchar(i + '0');
-                    putchar(j + '0');
-                    putchar(k + '0');
-                }
-                if (!(i == 7 && j == 8 && k == 9))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-		}
-	}
+			for (k = j + 1; k <= 9; k++)
+			{
+				if (i != j)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					}
+					if (!(i == 7 && j == 8 && k == 9))
+					{
+						putchar(',');
+						putchar(' ');
+						}
+						}
+						}
+						}
 	putchar('\n');
 	return (0);
 }
