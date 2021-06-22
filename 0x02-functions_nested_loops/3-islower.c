9a0d70e7656a298b35cd3368ae671d72d81b8cb4
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-22 19:23:21 +0100

    Betty please

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 
 /**
- * @c: The character or ascii code
  * _islower - checks for lowercase character
  * Return: 0 or 1
  */
@@ -12,9 +11,9 @@ int _islower(int c)
 
 	if (c >= 97 && c <= 122)
 		result = 1;
-		
-	if (c >= 65 && c <= 90)
-		result = 0;
 	
+	else
+		result = 0;
+
 	return (result);
 }
