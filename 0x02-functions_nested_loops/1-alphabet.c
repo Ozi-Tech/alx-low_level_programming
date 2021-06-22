06f4d8d865eeb8049c14929582f1d929bbf7157d
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-22 18:11:57 +0100

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * void print_alphabet(void) - function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ * Return:
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		_putchar(ch);
+	}
+	_putchar('\n');
+	return (0)
+}
