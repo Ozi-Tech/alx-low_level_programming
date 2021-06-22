98950e6b7371b0c22c38585315466e4369cca439
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-22 19:16:44 +0100

    Errors

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,16 +1,18 @@
 #include "holberton.h"
 
 /**
+ * @c: The character or ascii code
  * _islower - checks for lowercase character
- * Return: 1 if c is lowercase,
- * Return: 0 otherewise
+ * Return: 0 or 1
  */
 
 int _islower(int c)
 {
 	int result;
+
 	if (c >= 97 && c <= 122)
 		result = 1;
+		
 	if (c >= 65 && c <= 90)
 		result = 0;
 	
