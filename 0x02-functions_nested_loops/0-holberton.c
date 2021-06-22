6e084ea41ffd182dcd373c36c273ea592fb7f989
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-22 17:35:12 +0100

    Task 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -7,9 +7,16 @@
 
 int main ()
 {
-	char ch = "Holberton";
-
-	putchar(ch);
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('r');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
 
 	return(0);
 }
