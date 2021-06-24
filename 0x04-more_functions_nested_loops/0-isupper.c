166a4998b5e220bfa47b1ab6eb59c66cdbb4c022
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-24 18:00:46 +0100

    Uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,16 @@
 #include "holberton.h"
 
 /**
- * _islower - checks for lowercase character
+ * _isupper - checks for uppercase character
  * @c: character or ascii code
  * Return: 0 or 1
  */
 
-int _islower(int c)
+int _isupper(int c)
 {
-	int result;
-
 	if (c >= 65 && c <= 90)
-		result = 1;
+		return (1);
 
 	else
-		result = 0;
-
-	return (result);
+		return (0);
 }
