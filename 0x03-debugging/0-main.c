ddb2c64925828a50fbe93af4f9f97a1c403ba7cb
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-06-24 04:34:01 +0100

    Fix errors

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "holberton.h"
 
 /**
- * main - test that the function positive_or_negative() gives the correct output when given a case of 0
+ * main - test that the function positive_or_negative()
+ * gives the correct output when given a case of 0
  * Return: 0
  */
 
