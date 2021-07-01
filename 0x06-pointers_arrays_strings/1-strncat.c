d3fa61e01a7edeae23e614a0ea96bf6f167a38db
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-07-01 19:08:03 +0100

    errors

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,8 +12,8 @@ char *_strncat(char *dest, char *src, int n)
 	for (a = 0; dest[a] != '\0'; a++);
 	for (b = 0; b < n && src[b] != '\0'; a++, b++)
 	{
-		dest[a] = src[b];
+		dest[a + b] = src[b];
 	}
-	dest[a] = '\0';
+	dest[a + b] = '\0';
 	return (dest);
 }
