b830e59cc1c0f52c017dd96a2d0e74e0906d5b27
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-07-01 18:53:41 +0100

    using strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strncat -
+ * Return:
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++);
+	for (b = 0; b < n && src[b] != '\0'; a++, b++)
+	{
+		dest[a] = src[b];
+	}
+	dest[a] = '\0';
+	return (dest);
+}
