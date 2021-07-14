39a8cf60e3e62106ea415654e4a14a80597b9a39
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-07-14 20:30:40 +0100

    Malloc Project

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ **argstostr - concatenates all the arguments of your program
+ *@ac: parameter integer
+ *@av: parameter pointer
+ *Return: pointer or NULL
+ */
+
+char *argstostr(int ac, char **av)
+{
+
+}
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ *str_concat - concatenates all the arguments of your program
+ *@s1: parameter char
+ *@av: parameter char pointer
+ *Return: pointer or NULL
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
+}
