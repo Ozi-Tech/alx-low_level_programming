017bd19c5a3292cd0326a169f72ff9c1fcfdbe1d
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-07-15 21:15:30 +0100

    More malloc projects

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: parameter integer
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	*ptr = malloc(b);
+	if (*ptr == NULL)
+	{
+		exit(98);
+	}
+	return (*ptr);
+}
