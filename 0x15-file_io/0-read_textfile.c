c4e52188e29ac29b3e61c34086af17f063de4308
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-08-10 15:36:14 +0100

    Fix errors

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *buff = malloc(sizeof(char) * letters);
 
-	if (buff == NULL)
+	if (!filename)
+		return (0);
+
+	if (!buff)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
@@ -21,16 +24,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	r = read(fd, buff, letters);
 	if (r == -1)
+	{
+		free(buff);
 		return (0);
+	}
 
 	w = write(STDOUT_FILENO, buff, r)
 	if (w == -1)
+	{
+		free(buff);
 		return (0);
-	
+	}
+
 	if (r != w)
+	{
+		free(buff);
 		return (0);
+	}
 
 	free(buff);
-	close(fd);
+	c = close(fd);
+	if (c == -1)
+		return (0);
+
 	return (w);
 }
