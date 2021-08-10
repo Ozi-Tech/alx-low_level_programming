bfe224423e063bcd96a14c961eafc894f7b002fd
Ekene Ndubueze <ekene.ndubueze@gmail.com>
2021-08-10 21:56:27 +0100

    Fiz Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,16 +16,20 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *buff = malloc(sizeof(char) * letters);
+	int r;
+	int w;
+	int c;
+	char *buff;
 
 	if (!filename)
 		return (0);
 
-	if (!buff)
-		return (0);
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
 		return (0);
 
 	r = read(fd, buff, letters);
